7e37e168de20eeb83aaa14309f394861041495e8
phayes <patrick.d.hayes@gmail.com>
2019-04-17 16:08:31 -0700

    Adding CPU cycle counter

---
+++ src/cpucycles.rs
@@ -0,0 +1,8 @@
+#[link(name="src/cpucycles.c")]
+extern "C" {
+    pub fn unsafe_cpucycles() -> libc::int64_t;
+}
+
+pub fn cpucycles() -> u64 {
+    unsafe { unsafe_cpucycles() as u64 }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,59 @@
-pub mod optimizer;
+mod cpucycles;
+mod optimizer;
+mod stats;
+use optimizer::Optimizer;
+
+pub struct SideFuzz<T>
+where
+  T: Fn(&[u8]) -> Result<(), ()>,
+{
+  len: usize,
+  function: T,
+}
+
+impl<T> SideFuzz<T>
+where
+  T: Fn(&[u8]) -> Result<(), ()>,
+{
+  pub fn new(len: usize, function: T) -> Self {
+    SideFuzz {
+      len: len,
+      function: function,
+    }
+  }
+
+  pub fn run(&self) {
+    let mut time_optimizer = Optimizer::new(self.len, |first: &[u8], second: &[u8]| {
+      let cycle_count = cpucycles::cpucycles();
+      let result = (self.function)(first);
+      let first_cycles = cpucycles::cpucycles() - cycle_count;
+      if result.is_err() {
+        return 0.0;
+      }
+
+      let cycle_count = cpucycles::cpucycles();
+      let result = (self.function)(second);
+      let second_cycles = cpucycles::cpucycles() - cycle_count;
+      if result.is_err() {
+        return 0.0;
+      }
+
+      let ratio = first_cycles as f64 / second_cycles as f64;
+
+      ratio
+    });
+
+    // Print results once every 1000 genearations
+    loop {
+      for _ in 0..1000 {
+        time_optimizer.step();
+      }
+      let population = time_optimizer.population();
+      println!(
+        "{} {}",
+        hex::encode(&population[0].0),
+        hex::encode(&population[0].1)
+      );
+    }
+  }
+}
