07ae48792831c567af19416eab7dc4058817039c
phayes <patrick.d.hayes@gmail.com>
2019-04-19 02:28:48 -0700

    Basically working

--- src/dudect.rs
+++ src/dudect.rs
@@ -1,4 +1,3 @@
-use crate::cpucycles;
 use statrs::statistics::Mean;
 use statrs::statistics::Variance;
 
@@ -23,10 +22,11 @@ where
     }
   }
 
-  pub fn time_run(&self) {
+  pub fn run(&self) -> Result<f64, ()> {
     let mut first_samples: Vec<f64> = Vec::new();
     let mut second_samples: Vec<f64> = Vec::new();
 
+    let mut t: f64;
     loop {
       for _ in 0..100_000 {
         let timer = cpu_time::ProcessTime::now();
@@ -38,30 +38,22 @@ where
         second_samples.push(timer.elapsed().as_nanos() as f64);
       }
 
-      let t = calculate_t(&first_samples, &second_samples);
+      t = calculate_t(&first_samples, &second_samples);
 
       println!("samples: {}, t-value: {}", first_samples.len(), t);
-    }
-  }
 
-  pub fn cycle_run(&self) {
-    let mut first_samples: Vec<f64> = Vec::new();
-    let mut second_samples: Vec<f64> = Vec::new();
-
-    loop {
-      for _ in 0..100_000 {
-        let cycle_count = cpucycles::cpucycles();
-        (self.function)(&self.first).unwrap(); // Because inputs are verified, unwrap OK
-        first_samples.push((cpucycles::cpucycles() - cycle_count) as f64);
-
-        let cycle_count = cpucycles::cpucycles();
-        (self.function)(&self.second).unwrap(); // Because inputs are verified, unwrap OK
-        second_samples.push((cpucycles::cpucycles() - cycle_count) as f64);
+      if t > 3.2905 {
+        println!(
+          "Found timing difference with 99.99% confidence.\n{}\n{}",
+          hex::encode(self.first),
+          hex::encode(self.second)
+        );
+        return Ok(t);
       }
-
-      let t = calculate_t(&first_samples, &second_samples);
-
-      println!("samples: {}, t-value: {}", first_samples.len(), t);
+      if first_samples.len() > 1_000_000 && t < 0.253347 {
+        println!("Giving up on these inputs...");
+        return Err(());
+      }
     }
   }
 }
@@ -81,4 +73,36 @@ fn calculate_t(first: &[f64], second: &[f64]) -> f64 {
     / ((first_variance / sample_size) + (second_variance / sample_size)).sqrt();
 
   t
+}
+
+fn degrees_freedom(first: &[f64], second: &[f64]) -> f64 {
+  debug_assert!(first.len() == second.len());
+  debug_assert!(first.len() >= 2);
+
+  (first.len() - 1) as f64 + (second.len() - 1) as f64
+}
+
+fn p_value_from_t_value(t: f64) -> f64 {
+  // TODO: use formula instead of table.
+
+  // assume infinite degrees of freedom
+  let t_table = vec![
+    (3.2905, 0.0005),
+    (2.57583, 0.005),
+    (2.32635, 0.01),
+    (1.95996, 0.025),
+    (1.644854, 0.05),
+    (1.281552, 0.10),
+    (0.674490, 0.25),
+    (0.253347, 0.4),
+    (0.0, 0.0),
+  ];
+
+  for (t_value, p_value) in t_table {
+    if t > t_value {
+      return p_value;
+    }
+  }
+
+  panic!("Invalid t value");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -34,128 +34,127 @@ where
     }
   }
 
-  pub fn count_cycles(&self, input: &[u8]) -> u64 {
-    // Sample 5 times and take the average of the middle 3
-    let mut i = 5;
-    let mut cycle_counts: Vec<u64> = Vec::with_capacity(5);
+  #[no_mangle]
+  fn num_executions(&self) -> u64 {
+    // Find a good input
+    let mut input: Vec<u8>;
     loop {
-      let cycle_count = cpucycles::cpucycles();
-      let result = (self.function)(input);
-      cycle_counts.push(cpucycles::cpucycles() - cycle_count);
-      if result.is_err() {
-        continue;
-      } else {
-        i -= 1;
-      }
-      if i == 0 {
+      input = (0..self.len).map(|_| rand::random::<u8>()).collect();
+      let result = (self.function)(&input);
+      if result.is_ok() {
         break;
       }
     }
 
-    cycle_counts.sort();
+    let mut num_executions = 1;
+    loop {
+      let timer = cpu_time::ProcessTime::now();
+      for _ in 0..num_executions {
+        let _ = (self.function)(&input);
+      }
+      let time = timer.elapsed().as_nanos();
+      if time > 20_000 {
+        // Target of 20 microseconds per run
+        break;
+      } else {
+        num_executions = num_executions * 10;
+      }
+    }
 
-    // TODO: make this dynamic
-    let average = (cycle_counts[1] + cycle_counts[2] + cycle_counts[3]) / 3;
-
-    average
+    num_executions
   }
 
-  pub fn time_run(&self) {
+  #[no_mangle]
+  pub fn run(&self) {
     // Pin to a single core
     // TODO: for multi-core checking, pin to different cores
+    println!("Setting core affinity");
     let core_ids = core_affinity::get_core_ids().unwrap();
     core_affinity::set_for_current(core_ids[0]);
 
+    // Sample execution time so that we have at least 20 microseconds per run
+    println!("Determining number of executions per measurement");
+    let num_executions = self.num_executions();
+    println!("{} executions per measurement", num_executions);
+
     let mut time_optimizer = Optimizer::new(self.len, |first: &[u8], second: &[u8]| {
-      let timer = cpu_time::ProcessTime::now();
-      let result = (self.function)(first);
-      let first_time = timer.elapsed().as_nanos();
+      let mut result: Result<(), ()> = Ok(());
+
+      let cycles_marker = cpucycles::cpucycles();
+      for _ in 0..num_executions {
+        result = (self.function)(first);
+      }
+      let first_cycles = cpucycles::cpucycles() - cycles_marker;
       if result.is_err() {
         return 0.0;
       }
 
-      let timer = cpu_time::ProcessTime::now();
-      let result = (self.function)(second);
-      let second_time = timer.elapsed().as_nanos();
+      let cycles_marker = cpucycles::cpucycles();
+      for _ in 0..num_executions {
+        result = (self.function)(second);
+      }
+      let second_cycles = cpucycles::cpucycles() - cycles_marker;
       if result.is_err() {
         return 0.0;
       }
 
-      let ratio = first_time as f64 / second_time as f64;
+      let ratio = first_cycles as f64 / second_cycles as f64;
+
+      if ratio == std::f64::NAN {
+        return 0.0;
+      }
 
       ratio
     });
 
-    // Check results once every 1000 genearations
+    println!("Evolving candidate input pairs");
     let mut best = ScoredInputPair::default(); // defaults to score of zero.
+    let mut average: f64 = 0.0;
     loop {
+      // Check results once every 1000 genearations
       for _ in 0..1000 {
         time_optimizer.step();
       }
       let population = time_optimizer.scored_population();
+
+      // Calculate average
+      let sum: f64 = population.iter().fold(0.0, |mut sum, val| {
+        sum += val.score;
+        sum
+      });
+      let new_average = sum / (population.len() as f64);
+
       println!(
         "{} {} {}",
-        &population[0].score,
+        new_average,
         hex::encode(&population[0].pair.first),
         hex::encode(&population[0].pair.second)
       );
 
-      if population[0].score >= best.score {
+      if new_average >= average {
         best = population[0].clone();
+        average = new_average;
       } else {
+        println!("Found candidate input pair");
         println!(
           "Checking {} {}",
           hex::encode(&best.pair.first),
           hex::encode(&best.pair.second)
         );
-        let dudect = dudect::DudeCT::new(&best.pair.first, &best.pair.second, &self.function);
-        dudect.time_run();
-
-        break;
-      }
-    }
-  }
-
-  pub fn cycles_run(&self) {
-    // Pin to a single core
-    // TODO: for multi-core checking, pin to different cores
-    let core_ids = core_affinity::get_core_ids().unwrap();
-    core_affinity::set_for_current(core_ids[0]);
-
-    let mut cycle_optimizer = Optimizer::new(self.len, |first: &[u8], second: &[u8]| {
-      let first_cycles = self.count_cycles(first);
-      let second_cycles = self.count_cycles(second);
-      let ratio = first_cycles as f64 / second_cycles as f64;
-
-      ratio
-    });
-
-    // Check results once every 1000 genearations
-    let mut best = ScoredInputPair::default(); // defaults to score of zero.
-    loop {
-      for _ in 0..1000 {
-        cycle_optimizer.step();
-      }
-      let population = cycle_optimizer.scored_population();
-      println!(
-        "{} {} {}",
-        &population[0].score,
-        hex::encode(&population[0].pair.first),
-        hex::encode(&population[0].pair.second)
-      );
-
-      if population[0].score >= best.score {
-        best = population[0].clone();
-      } else {
-        println!(
-          "Checking {} {}",
-          hex::encode(&best.pair.first),
-          hex::encode(&best.pair.second)
-        );
-        let dudect = dudect::DudeCT::new(&best.pair.first, &best.pair.second, &self.function);
-        dudect.time_run();
-
-        break;
+        let dudect = dudect::DudeCT::new(&best.pair.first, &best.pair.second, |input: &[u8]| {
+          for _ in 0..num_executions {
+            let _ = &(self.function)(input);
+          }
+          Ok(())
+        });
+        let result = dudect.run();
+        if result.is_ok() {
+          break;
+        } else {
+          best = ScoredInputPair::default();
+          average = 0.0;
+          println!("Continuing to evolve candidate input pairs");
+        }
       }
     }
   }
